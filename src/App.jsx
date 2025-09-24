49d73309a894b2311cc74d9c435b38ed9561f9f8
Ahmed Elgharib <ghbalat446@gmail.com>
2025-09-24 20:31:34 +0300

    fix: corrected import path for TeacherRoutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ import AdminCategoryTable from "./pages/Admin/AdminCategories";
 import CategoryDetailPage from "./pages/Admin/AdminCategoryDetails";
 import CourseDetailPage from "./pages/student/courseDetails";
 import AdminRoutes from "./utils/AdminRoutes";
-import TeacherRoutes from "./utils/TeacherRoutes.jsx";
+import TeacherRoutes from "./utils/TeacherRoutes";
+
 
 import Login from "./pages/student/login";
 import Signup from "./pages/student/signUp";
